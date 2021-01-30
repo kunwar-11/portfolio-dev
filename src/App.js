0129ef4c987a3bf603055e710424492c8a961f60
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-01-30 20:47:38 +0530

    style : created and styles the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Globalstyle from './components/Globalstyle'
 import AboutPage from './pages/Aboutus'
+import Nav from './components/Nav'
 function App() {
   return (
     <div className="App">
       <Globalstyle />
+      <Nav />
       <AboutPage />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+function Nav() {
+    return (
+        <Navigation>
+           <h1><a id = 'logo' href="#">Capture</a></h1>
+           <ul>
+               <li>
+                   <a href="#">About us</a>
+               </li>
+               <li>
+                   <a href="#">Our work</a>
+               </li>
+               <li>
+                   <a href="#">Contact us</a>
+               </li>
+           </ul>  
+        </Navigation>
+    )
+}
+
+const Navigation = styled.nav`
+    min-height : 10vh;
+    display : flex;
+    justify-content : space-between;
+    align-items : center;
+    padding : 1rem 8rem;
+    background-color : #282828
+    a {
+        color : white;
+        text-decoration : none;
+    }
+    #logo{
+        font-size : 1.5rem;
+        font-family : 'lobster' , cursive;
+        font-weight : lighter;
+    }
+    ul {
+        display : flex;
+        list-style : none;
+    }
+    li {
+        padding-left : 10rem;
+        position : relative;
+    }
+`
+export default Nav
