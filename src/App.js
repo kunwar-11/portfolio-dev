8ca5edc968c4d6f672fc33ac3065e37c0d186ce3
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-01-30 12:43:59 +0530

    style : styled the about us section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Globalstyle from './components/Globalstyle'
 import AboutPage from './pages/Aboutus'
 function App() {
   return (
     <div className="App">
+      <Globalstyle />
       <AboutPage />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,58 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
 function AboutSection() {
     return (
-        <div className = 'aboutus'>
-            <div className="aboutus__title">
-                <div className="aboutus__title__part">
+        <Aboutus>
+            <AboutusTitle>
+                <AboutusTitlePart>
                     <h2>We work to make</h2>
-                </div>
-                <div className="aboutus__title__part">
+                </AboutusTitlePart>
+                <AboutusTitlePart>
                     <h2>your <span>dreams</span></h2>
-                </div>
-                <div className="aboutus__title__part">
+                </AboutusTitlePart>
+                <AboutusTitlePart>
                     <h2>come true</h2>
-                </div>
+                </AboutusTitlePart>
                 <p  className = 'aboutus__description'>
                     Contact us for photography or videography ideas you have, we have professionals with amzing skills 
                 </p>
                 <button className="aboutus__btn">Contact us</button>
-            </div>
-            <div className="aboutus__image">
+            </AboutusTitle>
+            <AboutusImage>
                 <img src={home1} alt="aboutus__img"/>
-            </div>
-        </div>
+            </AboutusImage>
+        </Aboutus>
     )
 }
+const Aboutus = styled.div`
+    min-height : 90vh;
+    display: flex;
+    align-items : center;
+    justify-content : space-between;
+    padding : 5rem 10rem;
+    color : white;
+`
 
+const AboutusTitle = styled.div`
+    flex : 1;
+    padding-right : 5rem;
+    h2 {
+        font-weight : lighter;
+    }
+`
+
+const AboutusImage = styled.div`
+    flex : 1;
+    overflow : hidden;
+    img {
+        width : 100%;
+        height : 80vh;
+        object-fit : cover;
+    }
+`
+
+const AboutusTitlePart = styled.div`
+    overflow : hidden;
+`
 export default AboutSection
